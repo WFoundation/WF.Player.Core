762f20fb1e3019d9ce2f34c78030e0e8db3fa4b8
Mangatome <cybisoft@free.fr>
2013-09-08 21:00:11 +0200

    Usings and notice cleaning.

--- Core/Model/Enums.cs
+++ Core/Model/Enums.cs
@@ -1,5 +1,7 @@
-﻿/// WF.Player.Core - A Wherigo Player Core for different platforms.
+﻿///
+/// WF.Player.Core - A Wherigo Player Core for different platforms.
 /// Copyright (C) 2012-2013  Dirk Weltz <web@weltz-online.de>
+/// Copyright (C) 2012-2013  Brice Clocher <contact@cybisoft.net>
 ///
 /// This program is free software: you can redistribute it and/or modify
 /// it under the terms of the GNU Lesser General Public License as
@@ -13,6 +15,7 @@
 /// 
 /// You should have received a copy of the GNU Lesser General Public License
 /// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+///
 
 using System;
 namespace WF.Player.Core
--- Core/Model/Events.cs
+++ Core/Model/Events.cs
@@ -1,4 +1,23 @@
-﻿using System;
+﻿///
+/// WF.Player.Core - A Wherigo Player Core for different platforms.
+/// Copyright (C) 2012-2013  Dirk Weltz <web@weltz-online.de>
+/// Copyright (C) 2012-2013  Brice Clocher <contact@cybisoft.net>
+///
+/// This program is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU Lesser General Public License as
+/// published by the Free Software Foundation, either version 3 of the
+/// License, or (at your option) any later version.
+/// 
+/// This program is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU Lesser General Public License for more details.
+/// 
+/// You should have received a copy of the GNU Lesser General Public License
+/// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+///
+
+using System;
 using System.Collections.Generic;
 
 namespace WF.Player.Core
